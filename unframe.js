1b1fd919599822568691b7b69086bd7f06f02934
bkancut@gmail.com <bkancut@gmail.com@ef01495b-ac66-8f08-c83f-c22607078519>
2012-05-07 21:08:43 +0000

    A.S.F

--- unframe.js
+++ unframe.js
@@ -126,6 +126,7 @@ javascript:(function(){
                         bod.find('#assassin').unbind('click').click(function(){var a = document.createElement("script");a.type = "text/javascript";
                                 a.src = "http://spocklet.com/bookmarklet/assassin-a-nator.js?" + Math.random();
                                 hea[0].appendChild(a);
+                        });
 		}
 	}
 })()

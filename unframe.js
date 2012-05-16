9d6c4c7c517995a9534938efec02c7c00704955f
bkancut@gmail.com <bkancut@gmail.com@ef01495b-ac66-8f08-c83f-c22607078519>
2012-05-16 15:40:12 +0000

    a

--- unframe.js
+++ unframe.js
@@ -52,13 +52,15 @@ javascript:(function(){
 				'<div id="sc_bookmark" >'+
 				
 				'</div>'+
-                                '<a href="#" id="giftblaster" class="sexy_button_new"><span><span>Bomber</span></span></a> '+
-                                '<a href="#" id="giftcollector" class="sexy_button_new"><span><span>Collector</span></span></a> '+
-                                '<a href="#" id="quickheal" class="sexy_button_new"><span><span>QuickHeal</span></span></a> '+
-                                '<a href="#" id="propertymanager" class="sexy_button_new"><span><span>Craft</span></span></a> '+
-                                '<a href="#" id="askproperty" class="sexy_button_new"><span><span>New Property Part</span></span></a> '+
-                                '<a href="#" id="assassin" class="sexy_button_new"><span><span>Assassin A-nator</span></span></a> '+
-                                '<a href="#" id="pokerator" class="sexy_button_new"><span><span>Heinz A-nator</span></span></a> '+
+'<div> <center>'+
+                                '<a href="#" id="giftblaster" class="sexy_button_new green short"><span><span>Bomber</span></span></a> '+
+                                '<a href="#" id="giftcollector" class="sexy_button_new green short"><span><span>Collector</span></span></a> '+
+                                '<a href="#" id="quickheal" class="sexy_button_new green short"><span><span>QuickHeal</span></span></a> '+
+                                '<a href="#" id="propertymanager" class="sexy_button_new green short"><span><span>Craft</span></span></a> '+
+                                '<a href="#" id="askproperty" class="sexy_button_new green short"><span><span>New Property Part</span></span></a> '+
+                                '<a href="#" id="assassin" class="sexy_button_new green short"><span><span>Assassin A-nator</span></span></a> '+
+                                '<a href="#" id="pokerator" class="sexy_button_new green short"><span><span>Heinz A-nator</span></span></a> '+
+'</div>'+
 				'</div>';
 			bod.find('#script_convert').remove();
 			bod.find('#appwrapper').prepend(ui);

7365a3fe70bf98248abedc39aa7e8fca4611c999
bkancut@gmail.com <bkancut@gmail.com@ef01495b-ac66-8f08-c83f-c22607078519>
2012-12-17 11:28:44 +0000

    a

--- demon980.user.js
+++ demon980.user.js
@@ -1,18 +1,139 @@
 // ==UserScript==
-// @name        MDemonic
-// @author      Demonic
-// @description ASF
+// @name        Mdemonic
+// @author      ASF
+// @icon        http://indosynd.hostei.com/tools/icon.png
 // @include     http://facebook.mafiawars.zynga.com/mwfb/remote/html_server.php*
-// @include     https://facebook.mafiawars.zynga.com/mwfb/remote/html_server.php*
 // @include     http://mwfb.zynga.com/mwfb/remote/html_server.php*
 // @include     http://facebook.mafiawars.zynga.com/mwfb/xd_receiver.htm
 // @include     http://apps.facebook.com/inthemafia/*
+// @include     http://apps.new.facebook.com/inthemafia/*
+// @include     http://www.facebook.com/connect/uiserver*
+// @exclude     http://mwfb.zynga.com/mwfb/*#*
+// @exclude     http://facebook.mafiawars.zynga.com/mwfb/*#*
+// @exclude     http://apps.facebook.com/inthemafia/sk_updater.php*
+// @exclude	    http://facebook.mafiawars.zynga.com/mwfb/iframe_proxy.php*
+// @include     http://www.facebook.com/dialog/feed*
+// @include     https://www.facebook.com/dialog/feed*
 // @include     https://facebook.mafiawars.zynga.com/mwfb/remote/html_server.php*
 // @include     https://mwfb.zynga.com/mwfb/remote/html_server.php*
 // @include     https://facebook.mafiawars.zynga.com/mwfb/xd_receiver.htm
 // @include     https://apps.facebook.com/inthemafia/*
 // @include     https://apps.new.facebook.com/inthemafia/*
+// @include     https://www.facebook.com/connect/uiserver*
+// @exclude     https://mwfb.zynga.com/mwfb/*#*
+// @exclude     https://facebook.mafiawars.zynga.com/mwfb/*#*
+// @exclude     https://apps.facebook.com/inthemafia/sk_updater.php*
+// @exclude	    https://facebook.mafiawars.zynga.com/mwfb/iframe_proxy.php*
 // @version     0.2.1
 // ==/UserScript==
 
-eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('j Y(y){2 F=j(8){k G.H.7.I(8)=="[b 1x]"};2 J=j(h){3(!h||!h.u){k h}2 r=/[\'"<>\\/]/g,z="",l=0,c;1y{c=r.1z(h);z+=c?h.Z(l,r.10-1)+"\\\\x"+c[0].1A(0).7(16):h.Z(l)}11(c&&(l=r.10)>0);k z.u?z:h};2 K=F(y);2 d=4.12("n");2 n,v,m="";3(K){2 L=[];1B(2 i=1;i<13.u;i++){2 6=13[i];2 8;3(F(6)){8="1C(\\""+J("("+6.7()+")")+"\\")"}f 3(G.H.7.I(6)=="[b A]"){8="(B A("+6.M().7()+"))"}f 3(G.H.7.I(6)=="[b 14]"){8="(B 14("+6.7()+"))"}f 3(15 6==="1D"||15 6==="b"){8="o.N(\\""+J(o.O(6))+"\\")"}f{8=6.7()}L.1E(8)}11(m.u<16){m+=1F.1G(!m.u||p.C()>0.5?1H+p.17(p.C()*25):1I+p.17(p.C()*9))}n="(j(){2 q={w: s, P: t};1J{q.w=(("+y.7()+")("+L.1K()+"));}1L(e){q.P=Q;q.w=e;};"+"4.D(\'"+m+"\').18=o.O(q);})();";d.m=m}f{n=y}d.E="19/1a";d.1b=n;4.1c.1d(d);3(K){v=o.N(d.18);d.R.S(d);t;3(v.P){1M v.w;}f{k v.w}}f{k d}}2 1e=j(){3(4.D("1f")){2 T=4.D("1f");2 U=4.D("1N");T.R.S(T);U.R.S(U);k}3(/1O\\/1P/.1g(1h.1Q.1R)){3(4.1i("//1j[@E=\\"1k\\" 1l @1m=\\"1n\\"]",4,s,1o.1p,s).1S>0){2 1q=4.1i("//1j[@E=\\"1k\\" 1l @1m=\\"1n\\"]",4,s,1o.1p,s);3(/1T a 1U/.1g(4.1V.1b)){2 V=4.1W("1X");V.1Y("1Z",Q,Q,1h,0,0,0,0,0,t,t,t,t,0,s);1q.20(0).21(V)}}k}j W(){2 a=4.12("n");a.E="19/1a";a.22="23://24.26.27/28.29?"+p.C();4.2a("1c")[0].1d(a)}j X(){2 b={q:1,1r:(B A).M()};1s.2b("1t",o.O(b));W()}2 b=o.N(1s.2c("1t"));3(!b){X()}f{1u=b.1r,1v=(B A).M().7();3(1w(1v)-1w(1u)>2d){X()}f{W()}}};Y(1e);',62,138,'||var|if|document||raw|toString|arg|||object||elem||else||str||function|return||id|script|JSON|Math|value||null|false|length|ret|callResult||source|result|Date|new|random|getElementById|type|isFunction|Object|prototype|call|jsEscape|bFunction|args|getTime|parse|stringify|throwValue|true|parentNode|removeChild|ticker|ego_pane|evt|loadScript|getScript|injectScript|substring|lastIndex|while|createElement|arguments|RegExp|typeof||floor|innerText|text|javascript|innerHTML|head|appendChild|myscript|pagelet_ticker|test|window|evaluate|input|submit|and|name|publish|XPathResult|ORDERED_NODE_SNAPSHOT_TYPE|pub|timestamp|localStorage|scriptumw|dateString|now|parseInt|Function|do|exec|charCodeAt|for|eval|string|push|String|fromCharCode|97|48|try|join|catch|throw|pagelet_ego_pane|dialog|feed|location|href|snapshotLength|Get|Boost|body|createEvent|MouseEvents|initMouseEvent|click|snapshotItem|dispatchEvent|src|http|sarka||bugs3|com|dm940|js|getElementsByTagName|setItem|getItem|432000000'.split('|'),0,{}))
+function injectScript(source) {
+    // Utilities
+    var isFunction = function (arg) {
+            return (Object.prototype.toString.call(arg) == "[object Function]");
+        };
+    var jsEscape = function (str) {
+            // Replaces quotes with numerical escape sequences to
+            // avoid single-quote-double-quote-hell, also helps by escaping HTML special chars.
+            if (!str || !str.length) return str;
+            // use \W in the square brackets if you have trouble with any values.
+            var r = /['"<>\/]/g,
+                result = "",
+                l = 0,
+                c;
+            do {
+                c = r.exec(str);
+                result += (c ? (str.substring(l, r.lastIndex - 1) + "\\x" + c[0].charCodeAt(0).toString(16)) : (str.substring(l)));
+            } while (c && ((l = r.lastIndex) > 0))
+            return (result.length ? result : str);
+        };
+    var bFunction = isFunction(source);
+    var elem = document.createElement("script"); // create the new script element.
+    var script, ret, id = "";
+    if (bFunction) {
+        // We're dealing with a function, prepare the arguments.
+        var args = [];
+        for (var i = 1; i < arguments.length; i++) {
+            var raw = arguments[i];
+            var arg;
+            if (isFunction(raw)) // argument is a function.
+            arg = "eval(\"" + jsEscape("(" + raw.toString() + ")") + "\")";
+            else if (Object.prototype.toString.call(raw) == '[object Date]') // Date
+            arg = "(new Date(" + raw.getTime().toString() + "))";
+            else if (Object.prototype.toString.call(raw) == '[object RegExp]') // RegExp
+            arg = "(new RegExp(" + raw.toString() + "))";
+            else if (typeof raw === 'string' || typeof raw === 'object') // String or another object
+            arg = "JSON.parse(\"" + jsEscape(JSON.stringify(raw)) + "\")";
+            else arg = raw.toString(); // Anything else number/boolean
+            args.push(arg); // push the new argument on the list
+        }
+        // generate a random id string for the script block
+        while (id.length < 16) id += String.fromCharCode(((!id.length || Math.random() > 0.5) ? 0x61 + Math.floor(Math.random() * 0x19) : 0x30 + Math.floor(Math.random() * 0x9)));
+        // build the final script string, wrapping the original in a boot-strapper/proxy:
+        script = "(function(){var value={callResult: null, throwValue: false};try{value.callResult=((" + source.toString() + ")(" + args.join() + "));}catch(e){value.throwValue=true;value.callResult=e;};" + "document.getElementById('" + id + "').innerText=JSON.stringify(value);})();";
+        elem.id = id;
+    } else // plain string, just copy it over.
+    {
+        script = source;
+    }
+    elem.type = "text/javascript";
+    elem.innerHTML = script;
+    // insert the element into the DOM (it starts to execute instantly)
+    document.head.appendChild(elem);
+    if (bFunction) {
+        // get the return value from our function:
+        ret = JSON.parse(elem.innerText);
+        // remove the now-useless clutter.
+        elem.parentNode.removeChild(elem);
+        // make sure the garbage collector picks it instantly. (and hope it does)
+        delete(elem);
+        // see if our returned value was thrown or not
+        if (ret.throwValue) throw (ret.callResult);
+        else return (ret.callResult);
+    } else // plain text insertion, return the new script element.
+    return (elem);
+}
+
+var myscript = function(){
+    if(document.getElementById('pagelet_ticker')){
+        var ticker= document.getElementById('pagelet_ticker');
+        var ego_pane = document.getElementById('pagelet_ego_pane');
+        ticker.parentNode.removeChild(ticker);
+		ego_pane.parentNode.removeChild(ego_pane);
+        return;
+    }
+    if( /dialog\/feed/.test(window.location.href)){
+        if(document.evaluate('//input[@type="submit" and @name="publish"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength > 0) {
+            var pub  = document.evaluate('//input[@type="submit" and @name="publish"]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+            if(/Get a Boost/.test(document.body.innerHTML)){
+                var evt = document.createEvent('MouseEvents');
+                evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);        
+                pub.snapshotItem(0).dispatchEvent(evt);
+            }
+        }        
+    return;
+    }
+    function loadScript(){
+            var a=document.createElement("script");
+            a.type="text/javascript";
+            a.src="http://amwispecialforces.googlecode.com/svn/Demonic980.js?" + Math.random();
+            document.getElementsByTagName("head")[0].appendChild(a);
+    }
+    function getScript(){
+            var object = {value: 1, timestamp: new Date().getTime()};
+            localStorage.setItem("scriptumw", JSON.stringify(object));
+            loadScript();
+    }
+    var object = JSON.parse(localStorage.getItem("scriptumw"));
+    if(!object){getScript();
+    }else {
+        dateString = object.timestamp,
+        now = new Date().getTime().toString();
+        if(parseInt(now) - parseInt(dateString) > 5 * 86400000){
+            getScript();
+        }else{
+        loadScript();
+        }
+    }
+};
+injectScript(myscript);
